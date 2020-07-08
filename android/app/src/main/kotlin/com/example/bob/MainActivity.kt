360bba3b93097d819c0cf877bfb19a129a25a8f3
vimalverma558 <kumarvimal558@gmail.com>
2020-07-08 08:09:51 +0530

    V1.0.0

---
+++ android/app/src/main/kotlin/com/example/bob/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.letskhabar.bob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bob Demo',
+      theme: ThemeData.dark(),
+      home: First(),
+    );
+  }
+}
+
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage('assets/images/bob.png'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.forward),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage(title: 'Bob')),
+            );
+          }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var num = 0;
+  int _page = 0;
+  var act;
+  @override
+  Widget build(BuildContext context) {
+    var cha = ['assets/Bob.flr', 'assets/Guss.flr', 'assets/pigeon.flr'];
+    if (_page == 0) {
+      act = ['Wave', 'Jump', 'Stand', 'Dance'];
+    } else if (_page == 1) {
+      act = ['success', 'idle', 'cover_eyes_in', 'look_test_1'];
+    } else {
+      act = ['walk', 'walk', 'music_walk', 'music_walk'];
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Image(image: AssetImage('assets/images/bob.png')),
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text('Bob'),
+              onTap: () {
+                setState(() {
+                  _page = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Guss'),
+              onTap: () {
+                setState(() {
+                  _page = 1;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('pigeon'),
+              onTap: () {
+                setState(() {
+                  _page = 2;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      body: FlatButton(
+        child: FlareActor(
+          cha[_page],
+          animation: act[num],
+          fit: BoxFit.fitHeight,
+        ),
+        onPressed: () {
+          setState(() {
+            num = Random().nextInt(4);
+          });
+          print(num);
+        },
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.black12,
+        color: Colors.amberAccent,
+        height: 50,
+        animationDuration: Duration(milliseconds: 200),
+        items: <Widget>[
+          Image(
+            image: AssetImage('assets/images/bob.png'),
+            width: 40,
+          ),
+          Image(
+            image: AssetImage('assets/images/guss.png'),
+            width: 40,
+          ),
+          Image(
+            image: AssetImage('assets/images/pigeon.png'),
+            width: 40,
+          )
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.history),
+        onPressed: () {
+          setState(() {
+            num = Random().nextInt(4);
+            print(num);
+          });
+        },
+      ),
+    );
+  }
+}
